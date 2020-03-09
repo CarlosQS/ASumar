af1ad12a8b1a49c3e68a7bf3f83402cd06ab96ca
CarlosQS <carlos.qs.01.cqs@gmail.com>
2020-03-09 09:33:17 +0100

    Empezando prueba

---
+++ src/ASumar.java
@@ -0,0 +1,26 @@
+
+public class ASumar {
+
+    private String valor_inicial;
+
+    /**
+     * Constructor de la clase ASumar.
+     * @param string Valor introducido
+     */
+	public ASumar(String string) {
+        this.valor_inicial = string;
+    }
+
+	/**
+	 * Método para mostrar el valor que le hemos dado desde el constructor.
+	 * @return Devuelve una cadena de texto de tipo String
+	 */
+    public String mostrar() {
+        String numero = valor_inicial;
+        if (numero.length() == 1) {
+            return (numero + " = " + numero);
+        }
+        return null;
+    }
+	
+}
