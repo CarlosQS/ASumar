74ee23f7df561b4580484c38b0ab76b810e5c9da
CarlosQS <carlos.qs.01.cqs@gmail.com>
2020-03-09 09:54:21 +0100

    Última prueba del proyecto. Resultado correcto.

--- test/TestASumar.java
+++ test/TestASumar.java
@@ -4,9 +4,12 @@ import org.junit.Test;
 
 public class TestASumar {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testUnaCifra() {
+        ASumar sumi = new ASumar("54");
+        String resultado = sumi.mostrar();
+        int resultadoValorSumado = sumi.total();
+        assertEquals("54 = "+resultadoValorSumado,resultado );
+    }
 
 }
